711a427a70fe1fb378bfd4df81ee4ede65cb7f58
shu blazing <forblazing@gmail.com>
2023-10-14 10:58:14 +0900

    最新化

--- SwitchAudioDevice.ps1
+++ SwitchAudioDevice.ps1
@@ -1,6 +1,6 @@
 $CurrentDevice = Get-AudioDevice -Playback
-if ($CurrentDevice.ID -eq "{0.0.0.00000000}.{95446616-b074-4912-b18f-6aff56a043bb}") {
-    Set-AudioDevice -ID "{0.0.0.00000000}.{abcd1234-efgh-5678-ijkl-9mnopqrstuv}"
+if ($CurrentDevice.ID -eq "{0.0.0.00000000}.{e3f8ff66-3a2a-4f4a-b7c3-98839f8641fb}") {
+    Set-AudioDevice -ID "{0.0.0.00000000}.{213e4854-06d4-473e-84f1-45e81e3768cc}"
 } else {
-    Set-AudioDevice -ID "{0.0.0.00000000}.{95446616-b074-4912-b18f-6aff56a043bb}"
+    Set-AudioDevice -ID "{0.0.0.00000000}.{e3f8ff66-3a2a-4f4a-b7c3-98839f8641fb}"
 }
